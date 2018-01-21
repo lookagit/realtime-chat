47b5dc447b1265fc343cc07fafa96199bec16e6d
Luka <simjanovic.luka@gmail.com>
2018-01-21 20:17:19 +0100

    initial commit

---
+++ reactNative/lukaDriver/Main/MapCont.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import Pusher from 'pusher-js/react-native';
+
+class MapCont extends Component {
+  state = {
+    message: 'Unknown',
+    name: 'Unknown',
+  }
+  constructor(props) {
+    super(props);
+    this.available_drivers_channel = null;
+    this.ride_channel = null;
+    this.pusher = null;
+  }
+
+  componentDidMount() {
+    this.pusher = new Pusher('6f48f906c45c9541f9da', {
+      cluster: 'eu',
+      encrypted: true
+    });
+
+    var channel = this.pusher.subscribe('my-channel');
+    channel.bind('my-event', (data) => {
+      this.setState({
+        message: data.message,
+        name: data.ime,
+      })
+      console.log("JA SAM DATAAAA ", data);
+    });
+  }
+  createTriger = async () => {
+    if(this.state.name !== 'Unknown' && this.state.message !== 'Unknown') {
+      let getServer = await fetch('https://server-hryyliokwv.now.sh', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          ime: this.state.name,
+          message: this.state.message,
+        }),
+      });
+    }
+  }
+  render() {
+    return(
+      <View>
+        <Text>{this.state.message}</Text>
+        <Text>{this.state.name}</Text>
+        <TextInput
+          placeholder="Unesite ime..."
+          onChangeText={(val) => {
+             this.setState({
+               name: val,
+             })
+          }}
+        />
+        <TextInput
+          placeholder="Unesite Poruku..."
+          multiline={true}
+          onChangeText={(val) => {
+             this.setState({
+               message: val,
+             })
+          }}
+        />
+        <TouchableOpacity onPress={() => {
+          this.createTriger();
+        }}>
+          <Text>AJDE KLIKNI</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+}
+export default MapCont;
---
+++ server/server.js
@@ -0,0 +1,39 @@
+require('now-logs')('nasaChatApp');
+var express = require('express');
+var bodyParser = require('body-parser');
+var Pusher = require('pusher');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var pusher = new Pusher({
+  appId: '460761',
+  key: '6f48f906c45c9541f9da',
+  secret: '7a7659b7463d25de4461',
+  cluster: 'eu',
+  encrypted: true,
+});
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+
+app.post('/', function(req, res) {
+  pusher.trigger('my-channel', 'my-event', {
+    "ime": req.body.name,
+    "message": req.body.message,
+  });
+  console.log('req ', req.body);
+})
+
+app.get('/', function(req, res) {
+  res.send("KAKO Jsadaddsadasd ");
+})
+
+
+
+var port = process.env.PORT || 5000;
+
+app.listen(port);
